44bdeda9504bf197631ddd091b0f005dae9b720d
Ron Struempf <rstruem1@my.westga.edu>
2016-01-20 20:01:50 -0500

    Profile version complete

--- app/src/main/java/com/ronstruempf/ronstruempfcontacts/MainActivity.java
+++ app/src/main/java/com/ronstruempf/ronstruempfcontacts/MainActivity.java
@@ -1,13 +1,14 @@
 package com.ronstruempf.ronstruempfcontacts;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                .setAction("Action", null).show();
+        //    }
+        //});
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                copyEditorToTextView(R.id.nameEdit, R.id.nameView);
+                copyEditorToTextView(R.id.emailEdit, R.id.emailView);
+                copyEditorToTextView(R.id.phoneEdit, R.id.phoneView);
+                RadioButton rb = (RadioButton)findViewById(R.id.phoneIsCell);
+                String text = "";
+                if (rb.isChecked()) {
+                    text = "(CELL)";
+                } else {
+                    rb = (RadioButton)findViewById(R.id.phoneIsLandLine);
+                    if (rb.isChecked()) {
+                        text = "Land Line";
+                    }
+                }
+                TextView textView = (TextView)findViewById(R.id.phoneTypeView);
+                textView.setText(text);
             }
         });
+
+    }
+
+    /**
+     * Copy an editor value to a text view for that item
+     *
+     * @param editorId View id for EditText to copy from
+     * @param textViewId View id of TextView to copy to
+     */
+    private void copyEditorToTextView(int editorId, int textViewId) {
+        EditText editText = (EditText)findViewById(editorId);
+        TextView textView = (TextView)findViewById(textViewId);
+        textView.setText(editText.getText());
     }
 
     @Override
